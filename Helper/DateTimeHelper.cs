a24cd9e2942569bd0650d30903d8d568417d18bd
dizzy <184835166+DizzyPho@users.noreply.github.com>
2025-10-16 18:35:05 +0200

    Add helper method to get valid date from user

--- Helper/DateTimeHelper.cs
+++ Helper/DateTimeHelper.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 
 namespace Helper
 {
     internal class DateTimeHelper
     {
+        public static DateTime GetValidDate(string query)
+        {
+            CultureInfo culture = new CultureInfo("nl-BE");
+            DateTime date;
+            while (true)
+            {
+                try
+                {
+                    string userGivenDate = UserInputHelper.StringInput(query);
+                    date = DateTime.Parse(userGivenDate, culture);
+                    break;
+
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid date. Please try again.");
+                    continue;
+                }
+            }
+            return date;
+        }
     }
 }
